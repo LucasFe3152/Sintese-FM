95c36cad83dd41a9f6d4f8b59caed78a2f4e0dbb
LucasFe3152 <lucas.satiro@dcomp.ufs.br>
2025-09-08 22:27:11 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,29 +1,74 @@
 from sintese import *
+from scipy.io import wavfile
 import sounddevice as sd
 
-freq_nota = 440.0
-duracao = 0.5
-f_amostragem = 44100
+A1 = 55
 
-t = np.linspace(0, duracao, int(f_amostragem * duracao), endpoint=False)
+INTERVALOS = {
+    'A': 0,
+    'B': 2,
+    'C': 3,
+    'D': 5,
+    'E': 7,
+    'F': 8,
+    'G': 10
+}
 
-i = 1
-while True:
-    sinal_fm = fm_batida(i, duracao, f_amostragem)
-    print(f'Frequência: {i}Hz')
-    sd.play(sinal_fm, f_amostragem)
+ACIDENTES = {
+    'n': 0,
+    's': 1,
+    'b': -1
+}
+
+F_AMOSTRAGEM = 44100
+
+with open("calcinhapreta.txt", "r", encoding="utf-8") as arquivo:
+            conteudo_total = arquivo.read()
+
+# musica = input()
+musica = conteudo_total
+
+musica = musica.split(',')
+config = musica[:2]
+duracao_padrao = float(config[0][1:])  # Convertendo para float
+instrumento = config[1]
+sons = musica[2:]
+duracao = 0.0
+som_final = []
+
+def get_freq(nota, acidente, oitava):
+    return A1 * (2 ** ((12 * oitava-1 + INTERVALOS[nota] + acidente)/12))
+
+def get_intensidade(intensidade):
+    if intensidade == 'F':
+        return 1
+    else:
+        return 0.5
+    
+def get_som(freq, duracao, intensidade, instrumento):
+    if instrumento == 'Ic':
+        return intensidade * fm_corda(freq, duracao, F_AMOSTRAGEM)
+    elif instrumento == 'Is':
+        return intensidade * fm_sopro(freq, duracao, F_AMOSTRAGEM)
+    else:
+        return intensidade * fm_batida(freq, duracao_padrao, F_AMOSTRAGEM)
+
+
+for som in sons:
+    nota = som[0]
+    acidente = ACIDENTES[som[1]]
+    oitava = som[2]
+    if instrumento == 'Ip':
+        duracao = int(duracao_padrao)
+    else:
+        duracao = float(duracao_padrao) / float(som[3])
+    intensidade = get_intensidade(som[4])
+
+    freq = get_freq(nota, acidente, int(oitava))
+    sinal = get_som(freq, duracao, intensidade, instrumento)
+    som_final.append(sinal)
+    sd.play(sinal, F_AMOSTRAGEM)
     sd.wait()
-    i += 10
-    if (i > 300):
-        break
 
-plt.figure(figsize=(15, 10))
-
-plt.subplot(3, 1, 1)
-plt.plot(t[:5000], sinal_fm[:5000])
-plt.title('Sinal de Síntese FM')
-plt.xlabel('Tempo (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-
-plt.show()
+musica_final = np.concatenate(som_final)
+wavfile.write('sintese_fm.wav', F_AMOSTRAGEM, musica_final.astype(np.float32))
